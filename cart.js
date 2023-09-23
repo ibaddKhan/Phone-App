4239116f23dc174f557be9fccaeec9dee87d6dd8
ibadurrehman718 <ibadurrehman718@gmail.com>
2023-09-23 05:29:12 +0500

    Error Solved

--- cart.js
+++ cart.js
@@ -5,9 +5,11 @@ print();
 function print() {
   for (let i = 0; i < items.length; i++) {
     //   items[i].Total = `${items[i].Price * items[i].Quantity;}`
+    // items[i].total = +`${items[i].price * items[i].Quantity}`;
     items[i].total = +`${items[i].price * items[i].Quantity}`;
+
     mainDiv.innerHTML += `
-      <div class="main"><div> 
+      <div class="main"><div>
       <h2 class=" padding:10px">${items[i].brand}</h2>
      <img src="${items[i].img}" class="image">
      <h3>Device : ${items[i].model}</h3>
@@ -16,7 +18,8 @@ function print() {
      <h3>Camera : ${items[i].camera}</h3>
      <h3>Quantity : ${items[i].Quantity}</h3>
      <h3>Price : <span style= "color:red; font-weight:bolder; font-size:20px;">${items[i].price}</span></h3>
-     <div style="display:flex; justify-content:center; gap:9px;" class= "add-sub"><button onclick="plus(${i})" style="margin:4px; padding:7px 20px;border-radius: 3px;" >+</button><h3>Total : ${items[i].total}</h3><button onclick="minus()" style="margin:4px; padding:7px 20px;border-radius: 3px;">-</button ></div>
+     <h3>Total : ${items[i].total}</h3>
+     <div style="display:flex; justify-content:center; gap:9px;" class= "add-sub"><button onclick="plus(${i})" style="margin:4px; padding:7px 20px;border-radius: 3px;" >+</button><button onclick="minus(${i})" style="margin:4px; padding:7px 20px;border-radius: 3px;">-</button ></div>
       </div>
       </div>`;
   }
@@ -26,9 +29,10 @@ function plus(index) {
   mainDiv.innerHTML = "";
   for (let i = 0; i < items.length; i++) {
     //   items[i].Total = `${items[i].Price * items[i].Quantity;}`
+    // items[i].total = +`${items[i].price * items[i].Quantity}`;
     items[i].total = +`${items[i].price * items[i].Quantity}`;
     mainDiv.innerHTML += `
-        <div class="main"><div> 
+        <div class="main"><div>
         <h2 class=" padding:10px">${items[i].brand}</h2>
        <img src="${items[i].img}" class="image">
        <h3>Device : ${items[i].model}</h3>
@@ -37,7 +41,9 @@ function plus(index) {
        <h3>Camera : ${items[i].camera}</h3>
        <h3>Quantity : ${items[i].Quantity}</h3>
        <h3>Price : <span style= "color:red; font-weight:bolder; font-size:20px;">${items[i].price}</span></h3>
-       <div style="display:flex; justify-content:center; gap:9px;" class= "add-sub"><button onclick="plus(${i})" style="margin:4px; padding:7px 20px;border-radius: 3px;" >+</button><h3>Total : ${items[i].total}</h3><button onclick="minus(${i})" style="margin:4px; padding:7px 20px;border-radius: 3px;">-</button ></div>
+       <h3>Total : ${items[i].total}</h3>
+       <div style="display:flex; justify-content:center; gap:9px;" class= "add-sub"><button onclick="plus(${i})" style="margin:4px; padding:7px 20px;border-radius: 3px;" >+</button><button onclick="minus(${i})" style="margin:4px; padding:7px 20px;border-radius: 3px;">-</button ></div>
+
         </div>
         </div>`;
   }
@@ -45,18 +51,46 @@ function plus(index) {
 function minus(index) {
   mainDiv.innerHTML = "";
   items[index].Quantity -= 1;
+
   for (let i = 0; i < items.length; i++) {
+    items[i].total = +`${items[i].price * items[i].Quantity}`;
+
     mainDiv.innerHTML += `
-        <div class="main"><div> 
+        <div class="main"><div>
         <h2 class=" padding:10px">${items[i].brand}</h2>
        <img src="${items[i].img}" class="image">
        <h3>Device : ${items[i].model}</h3>
-       <h3>Ram : ${items[i].ram} Gb</h3>s
+       <h3>Ram : ${items[i].ram} Gb</h3>
        <h3>Rom : ${items[i].rom}Gb</h3>
        <h3>Camera : ${items[i].camera}</h3>
        <h3>Quantity : ${items[i].Quantity}</h3>
        <h3>Price : <span style= "color:red; font-weight:bolder; font-size:20px;">${items[i].price}</span></h3>
-       <div style="display:flex; justify-content:center; gap:9px;" class= "add-sub"><button onclick="plus(${i})" style="margin:4px; padding:7px 20px;border-radius: 3px;" >+</button><h3>Total : ${items[i].total}</h3><button onclick="minus(${i})" style="margin:4px; padding:7px 20px;border-radius: 3px;">-</button ></div>
+       <h3>Total : ${items[i].total}</h3>
+       <div style="display:flex; justify-content:center; gap:9px;" class= "add-sub"><button onclick="plus(${i})" style="margin:4px; padding:7px 20px;border-radius: 3px;" >+</button><button onclick="minus(${i})" style="margin:4px; padding:7px 20px;border-radius: 3px;">-</button ></div>
+
+        </div>
+        </div>`;
+  }
+}
+
+function delte(index) {
+  retArray.splice(index, 1);
+  for (let i = 0; i < items.length; i++) {
+    items[i].total = +`${items[i].price * items[i].Quantity}`;
+
+    mainDiv.innerHTML += `
+        <div class="main"><div>
+        <h2 class=" padding:10px">${items[i].brand}</h2>
+       <img src="${items[i].img}" class="image">
+       <h3>Device : ${items[i].model}</h3>
+       <h3>Ram : ${items[i].ram} Gb</h3>
+       <h3>Rom : ${items[i].rom}Gb</h3>
+       <h3>Camera : ${items[i].camera}</h3>
+       <h3>Quantity : ${items[i].Quantity}</h3>
+       <h3>Price : <span style= "color:red; font-weight:bolder; font-size:20px;">${items[i].price}</span></h3>
+       <h3>Total : ${items[i].total}</h3>
+       <div style="display:flex; justify-content:center; gap:9px;" class= "add-sub"><button onclick="plus(${i})" style="margin:4px; padding:7px 20px;border-radius: 3px;" >+</button><button onclick="minus(${i})" style="margin:4px; padding:7px 20px;border-radius: 3px;">-</button ></div>
+
         </div>
         </div>`;
   }
