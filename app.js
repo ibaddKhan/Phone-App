da2474ec73ba5617090039e434722f5bcb67aa53
ibadurrehman718 <ibadurrehman718@gmail.com>
2023-10-11 02:54:21 +0500

    All Done Beaaauutifuly

--- app.js
+++ app.js
@@ -130,35 +130,38 @@ function cart(index) {
   }
   console.log(pArray);
 }
-
+// !Cartt button
 function gotoCart() {
   const cartItems = JSON.stringify(pArray);
   localStorage.setItem("pArray", cartItems);
   window.location = "./cart.html";
 }
+// !Login button Rendering
 
 const login = document.querySelector(".login");
-const signup = document.querySelector(".signup");
 const mainDiv2 = document.querySelector("#main-div2");
 
-login.addEventListener("click", clickingLogin);
+login.addEventListener("click", function () {
+  setTimeout(clickingLogin, 330);
+});
+
 function clickingLogin() {
   console.log("Login Working");
   mainDiv2.innerHTML = `  <div class="center">
   <div class="container">
   <label style="display:flex; justify-content:end; cursor:pointer;" onclick="remove();" class="close-btn fas fa-times" title="close"></label>
-  <div class="text">Login Form</div>
+  <div class="text">Login </div>
   <form action="#">
     <div class="data">
-      <label>Email or Phone</label>
-      <input class="email" type="text" required />
+      <label>Username or Email  </label>
+      <input  class="email" type="text"  required />
     </div>
     <div class="data">
       <label>Password</label>
       <input class="password" type="password" required />
     </div>
     <div class="forgot-pass">
-      <a href="#">Forgot Password?</a>
+      <a onclick="signing()">Create account?</a>
     </div>
     <div class="btn">
       <div class="inner"></div>
@@ -177,24 +180,128 @@ function clickingLogin() {
   const form = document.querySelector("form");
   const email = document.querySelector(".email");
   const pass = document.querySelector(".password");
-  const detailsArray = [];
-
+  // email.focus();
+  let userD = JSON.parse(localStorage.getItem("userD"));
   form.addEventListener("submit", sub);
+
   function sub(e) {
     e.preventDefault();
-    const sum = {
-      Email: email.value,
-      Pass: pass.value,
-    };
-    detailsArray.push(sum);
-    const userDetails = JSON.stringify(detailsArray);
-    localStorage.setItem("dArray", userDetails);
-    console.log(detailsArray);
+    let found = false; // Initialize a flag to check if a matching user is found
+
+    for (let i = 0; i < userD.length; i++) {
+      if (email.value == userD[i].email && pass.value == userD[i].Cpass) {
+        found = true; // Set the flag to true if a matching user is found
+        break; // Exit the loop since a match is found
+      }
+    }
+
+    if (found) {
+      Swal.fire({
+        position: "top-center",
+        icon: "success",
+        title: "Logging in",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+    } else {
+      Swal.fire({
+        position: "top-center",
+        icon: "error",
+        title: "Not an Account",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+    }
+
+    form.reset();
   }
 }
+// !Signup button Rendering
+const signup = document.querySelector(".signup");
+signup.addEventListener("click", function () {
+  setTimeout(signing, 330);
+});
 
+function signing() {
+  console.log("Signup Working");
+  mainDiv2.innerHTML = `  <div class="center con">
+  <div class="container main-con">
+  <label style="display:flex; justify-content:end; cursor:pointer;" onclick="remove();" class="close-btn fas fa-times" title="close"></label>
+  <div class="text">Sign Up</div>
+  <form action="#">
+  <div class="data" id="less-h">
+  <label>Username</label>
+  <input class="username"  type="text" required />
+  </div>
+  <div class="data "  id="less-h" >
+    <label>Email or Phone</label>
+    <input class="phone " type="text" required />
+  </div>
+    <div class="data "  id="less-h" >
+      <label>Password</label>
+      <input class="pas " type="password" required />
+    </div>
+    <div class="data "  id="less-h" >
+      <label>Confirm Password</label>
+      <input class="Cpas" type="password" required />
+    </div>
+    <div class="forgot-pass">
+      <a onclick="clickingLogin()">Already Have an Account</a>
+    </div>
+    <div class="btn">
+      <div class="inner"></div>
+      <button type="submit">Create</button>
+    </div>
+
+    </form>
+   </div>
+</div>`;
+  if (mainDiv2.innerHTML === "") {
+    mainDiv.style.opacity = 10000000;
+  } else {
+    mainDiv.style.opacity = 0.2;
+  }
+  const form = document.querySelector("form");
+  form.addEventListener("submit", create);
+
+  function create(e) {
+    e.preventDefault();
+    console.log("Working");
+
+    // Retrieve existing user data from local storage
+    const existingData = localStorage.getItem("userD");
+    const signD = existingData ? JSON.parse(existingData) : [];
+    const username = document.querySelector(".username");
+    const email = document.querySelector(".phone");
+    const pass = document.querySelector(".pas");
+    const Cpass = document.querySelector(".Cpas");
+
+    const userDetails = {
+      username: username.value,
+      email: email.value,
+      pass: pass.value,
+      Cpass: Cpass.value,
+    };
+
+    if (pass.value === Cpass.value) {
+      signD.push(userDetails);
+      localStorage.setItem("userD", JSON.stringify(signD));
+      form.reset();
+      Swal.fire({
+        position: "top-center",
+        icon: "success",
+        title: "Account Created",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+    } else {
+      console.log("Passwords are not the same");
+      alert("Passwords are not the same");
+    }
+  }
+}
 function remove() {
-  console.log("Ads");
+  console.log("Remove Working");
   mainDiv2.remove();
   location.reload();
 }
